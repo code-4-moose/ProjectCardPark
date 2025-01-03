7c8da55a48462caceb58e0ae1f3ca34098db4244
David <davidleebk@gmail.com>
2025-01-03 18:10:01 +0800

    Adding Card Image Enlargement

---
+++ ProjectCardPark/wwwroot/Card_Enlargement.js
@@ -0,0 +1,12 @@
+﻿function ImageEnlarger() {
+    document.querySelectorAll('.card-listings .card-img img').forEach(image => {
+        image.onclick = () => {
+            document.querySelector('.popup-image').style.display = 'block';
+            document.querySelector('.popup-image img').src = image.getAttribute('src');
+        }
+    });
+
+    document.querySelector('.popup-image span').onclick = () => {
+        document.querySelector('.popup-image').style.display = 'none';
+    }
+}
